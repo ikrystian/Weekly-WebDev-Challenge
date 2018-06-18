a0ac3e3336d158b5cc0eaeaa8f4b83c9aadf9571
Krystian <kkuzminski@betacom.com.pl>
2018-06-18 23:35:15 +0200

    Select list

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,16 @@
-document.addEventListener('DOMContentLoaded', function(){ 
+document.addEventListener('DOMContentLoaded', () => {
     setTimeout(function() {
         document.getElementById('loader').remove();
     }, 1);
 }, false);
 
+var cityInput = document.getElementById('city-value');
+cityInput.addEventListener('click', () => {
+    cityInput.classList.add('open');
+});
+document.querySelectorAll('.city-item').forEach(e => {
+    e.addEventListener('click', () => {
+        cityInput.value = e.innerHTML;
+        cityInput.classList.remove('open');
+    })
+});
